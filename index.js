0ed19ad35a5d3fe02e09c7fd0f3644eae6460af0
wale-bant <wale.ashiru04@gmail.com>
2020-05-20 14:54:44 +0100

    cors!

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ mongoose.connect(
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   })
+// );
 
 // Route middlewares
 app.use('/api/user', authRouter);
