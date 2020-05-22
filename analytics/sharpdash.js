2446ed691a4ec19d2e6ea7319c908a3815677142
wale-bant <wale.ashiru04@gmail.com>
2020-05-22 12:08:42 +0100

    integrated sharpdashboard

--- analytics/sharpdash.js
+++ analytics/sharpdash.js
@@ -1,11 +1,19 @@
 import { SharpDashBoard } from 'sharpdashboard-sdk-node';
 import User from '../models/User.js';
-import Video from '../models/Video.js';
+import Video from '../models/Video';
 
 const getUsers = async () => {
   try {
     const users = await User.find();
-    return users;
+    const filteredUsers = [];
+    users.forEach(user =>
+      filteredUsers.push({
+        username: user.username,
+        id: user._id,
+        date: user.date,
+      })
+    );
+    return filteredUsers;
   } catch (error) {
     console.log(error.message);
   }
@@ -14,13 +22,23 @@ const getUsers = async () => {
 const getVideos = async () => {
   try {
     const videos = await Video.find();
-    return videos;
+    const filteredVideos = [];
+    videos.forEach(vid =>
+      filteredVideos.push({
+        title: vid.title,
+        comments: vid.comments.length,
+        id: vid._id,
+      })
+    );
+    return filteredVideos;
   } catch (error) {
     console.log(error.message);
   }
 };
 
-const dashboard = new SharpDashBoard('api_key_here');
+const dashboard = new SharpDashBoard(
+  'eb8ad2e1d066a0e0e152deacfdc47d9afcaf813d'
+);
 
 dashboard.addModelHandler('users', getUsers);
 dashboard.addModelHandler('videos', getVideos);
